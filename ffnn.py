f2a06528a9086cd0397a4edbc4422f6db146b30d
Shreyansh Pathak <21f1003357@student.onlinedegree.iitm.ac.in>
2024-03-02 12:44:57 +0530

    building feed forward neural network

---
+++ ffnn.py
@@ -0,0 +1,19 @@
+from layer import *
+
+class FFNN:
+
+  def __init__(self,nin,nouts):
+
+    size = [nin] + nouts
+    self.layers = [Layer(size[i],size[i+1]) for i in range(len(nouts))]
+
+  def __call__(self,x):
+
+    for layer in self.layers:
+      x = layer(x)
+
+    return x 
+
+  def parameters(self):
+
+    return [parameter for layer in self.layers for parameter in layer.parameters()]
